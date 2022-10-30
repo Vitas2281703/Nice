07e5081e6ccfaa22bf7c8882f8dce8e9566a7a1a
puls300 <puls300@yandex.ru>
2022-10-30 19:03:10 +0700

    feat: Добавлена административная панель twill

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         DB::table('twill_users')->insert([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password' => Hash::make('1234'),
+             'role' => 'admin'
+         ]);
     }
 }
