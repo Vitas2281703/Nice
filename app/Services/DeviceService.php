7c6da8e44cfc80d0428360bb33205f5a4788c99d
Vitas2281703 <veretenov.vit@gmail.com>
2022-11-30 21:30:09 +0700

    feat: Добавлен прайс-лист с фильтром

--- app/Services/DeviceService.php
+++ app/Services/DeviceService.php
@@ -23,12 +23,18 @@ class DeviceService implements Contracts\DeviceService
         return $deviceOptions;
     }
 
+<<<<<<< HEAD
+    public function getDevicesByCategory($categoryID){
+        dd($this->repository->getDevicesByCategory($categoryID)->fabricator);
+        return $this->repository->getDevicesByCategory($categoryID);
+=======
     public function list(){
         return $this->repository->get();
     }
 
     public function getFilteredDevices($data){
         return $this->repository->getFilteredDevices($data);
+>>>>>>> e6ff881 (feat: Добавлен прайс-лист с фильтром)
     }
 
 
