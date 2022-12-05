6efd14ca0da4f0f4977584b746fa379b9e4e3e92
GornDanil <puls300@yandex.ru>
2022-12-05 22:09:23 +0700

    feat: Добавил необходимые колонки в бд

---
+++ database/migrations/2022_12_05_215943_add_column_in_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('referer_id')->nullable();
+            $table->foreign('referer_id')->references('id')->on('users');
+            $table->integer('bonus')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('bonus');
+        });
+    }
+};
